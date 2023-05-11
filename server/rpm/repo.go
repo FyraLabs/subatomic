f773faab928556835401fd5b733878f586c06835
Lleyton Gray <lleyton@fyralabs.com>
2023-05-11 00:11:48 -0700

    fix: pass --local-sqlite to createrepo_c fixing sqlite md generation on NFS

--- server/rpm/repo.go
+++ server/rpm/repo.go
@@ -24,7 +24,7 @@ func CreateRepo(repoPath string) error {
 }
 
 func UpdateRepo(repoPath string) error {
-	flags := []string{"--update", "--zck", "--xz"}
+	flags := []string{"--update", "--zck", "--xz", "--local-sqlite"}
 
 	_, err := os.Stat(path.Join(repoPath, "comps.xml"))
 	if err != nil && !os.IsNotExist(err) {
