f8b188a1d072dd4b3999ff04682d005e6fe3246f
Cappy Ishihara <cappy@cappuchino.xyz>
2025-09-26 13:20:01 +0700

    feat: optional appstream data in repomd

--- go.mod
+++ go.mod
@@ -31,6 +31,7 @@ require (
 	go.opentelemetry.io/otel/sdk v1.32.0
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
 	golang.org/x/sync v0.10.0
+	gopkg.in/ini.v1 v1.67.0
 )
 
 require (
@@ -106,6 +107,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241104194629-dd2ea8efbc28 // indirect
 	google.golang.org/grpc v1.67.1 // indirect
 	google.golang.org/protobuf v1.35.1 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server/rpm/repo.go
+++ server/rpm/repo.go
@@ -12,7 +12,28 @@ import (
 
 	pgp "github.com/ProtonMail/gopenpgp/v2/crypto"
 	"github.com/sassoftware/go-rpmutils"
+	"gopkg.in/ini.v1"
 )
+
+type MRepoBatchFile struct {
+	AppStreamData MRepoCBatchData `ini:"appstream"`
+	Icons         MRepoCBatchData `ini:"appstream-icons"`
+}
+
+// TOML struct for modifyrepo_c batch scripts
+//
+// [path/to/file]
+// ...
+type MRepoCBatchData struct {
+	Path              string `ini:"path"`
+	Type              string `ini:"type,omitempty"`
+	Remove            bool   `ini:"remove,omitempty"`
+	Compress          bool   `ini:"compress"`
+	CompressType      string `ini:"compress-type,omitempty"`
+	Checksum          string `ini:"checksum,omitempty"`
+	UniqueMdFileNames bool   `ini:"unique-md-filenames,omitempty"`
+	NewName           string `ini:"new-name"`
+}
 
 func CreateRepo(repoPath string) error {
 	if err := os.MkdirAll(repoPath, os.ModePerm); err != nil {
@@ -87,6 +108,82 @@ func writeTetsudouMetadata(repoPath string) error {
 		return err
 	}
 
+	return nil
+}
+
+// use `modifyrepo_c` to update AppStream metadata in the repo
+//
+// Expects the base path of the repo to be <repo>/latest, with the tree structure of:
+// <repo>/latest/
+//
+//	   appstream/
+//			<repo>.xml.gz
+//			<repo>-icons.tar.gz
+//	   icons/
+//			x64x64/
+//				<icon files>
+//			x128x128/
+//				<icon files>
+func MrepoCConfig(repoPath string, appstreamPath string) (*string, error) {
+	repoName := path.Base(repoPath)
+
+	batchTemplate := MRepoCBatchData{
+		Compress: true,
+	}
+
+	// [appstream]
+	appstreamConfig := batchTemplate
+	appstreamFile := path.Join(appstreamPath, repoName, "latest/appstream", fmt.Sprintf("%s.xml.gz", repoName))
+	appstreamConfig.Path = appstreamFile
+	appstreamConfig.NewName = "appstream.xml"
+
+	// [icons]
+	iconsConfig := batchTemplate
+	iconsFile := path.Join(appstreamPath, repoName, "latest/appstream", fmt.Sprintf("%s-icons.tar.gz", repoName))
+	iconsConfig.Path = iconsFile
+	iconsConfig.NewName = "appstream-icons.tar"
+
+	repoBatch := MRepoBatchFile{
+		// AppStreamData: appstreamConfig,
+		// Icons:         iconsConfig,
+	}
+	if _, err := os.Stat(iconsFile); err == nil {
+		repoBatch.Icons = iconsConfig
+	}
+
+	if _, err := os.Stat(appstreamFile); err == nil {
+		repoBatch.AppStreamData = appstreamConfig
+	}
+	ini.PrettyFormat = false
+
+	inifile := ini.Empty()
+	inifile.ReflectFrom(&repoBatch)
+
+	configFileName := fmt.Sprintf("%s-mrepoc.ini", repoName)
+	configPath := path.Join("/tmp", configFileName)
+	if err := inifile.SaveTo(configPath); err != nil {
+		return nil, err
+	}
+
+	return &configPath, nil
+}
+
+func ModifyRepoAppStream(repoPath string, appstreamPath string) error {
+	configPath, err := MrepoCConfig(repoPath, appstreamPath)
+	if err != nil {
+		return err
+	}
+
+	// log("Using mrepo_c config at", *configPath)
+	repodataDir := path.Join(repoPath, "repodata")
+	flags := []string{"-f", *configPath, repodataDir}
+
+	if _, err := exec.Command("modifyrepo_c", flags...).Output(); err != nil {
+		fmt.Println("modifyrepo_c error:", err)
+		return err
+	}
+
+	defer os.Remove(*configPath)
 	return nil
 }
 
