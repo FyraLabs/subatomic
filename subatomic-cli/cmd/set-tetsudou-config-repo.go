ee8182413de1f42d0fdaeab5d1e3a48b572c5a15
Lleyton Gray <lleyton@fyralabs.com>
2026-02-10 10:49:07 -0800

    add new set-tetsudou-config subcommand

---
+++ subatomic-cli/cmd/set-tetsudou-config-repo.go
@@ -0,0 +1,82 @@
+package cmd
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/FyraLabs/subatomic/server/types"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var repoSetTetsudouConfigCmd = &cobra.Command{
+	Use:   "set-tetsudou-config [repo_id] [tetsudou_url] [tetsudou_token]",
+	Short: "Set a Tetsudou config for a repo",
+	Args:  cobra.ExactArgs(3),
+
+	RunE: func(cmd *cobra.Command, args []string) error {
+		server := viper.GetString("server")
+		token := viper.GetString("token")
+
+		if server == "" {
+			return errors.New("server must be defined")
+		}
+
+		if token == "" {
+			return errors.New("token must be defined")
+		}
+
+		payload := types.SetTetsudouConfigPayload{
+			Url:   args[1],
+			Token: args[2],
+		}
+
+		data, err := json.Marshal(payload)
+		if err != nil {
+			return err
+		}
+
+		req, err := http.NewRequest(http.MethodPut, server+"/repos/"+args[0]+"/tetsudou", bytes.NewReader(data))
+		if err != nil {
+			return err
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Add("Accept", "application/json")
+		req.Header.Add("Authorization", "Bearer "+token)
+
+		client := &http.Client{}
+		res, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+
+		if res.StatusCode != http.StatusNoContent {
+			var serverError types.ErrResponse
+			if err := json.NewDecoder(res.Body).Decode(&serverError); err != nil {
+				return err
+			}
+
+			return fmt.Errorf("API returned error: %s", serverError.ErrorText)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	repoCmd.AddCommand(repoSetTetsudouConfigCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// repoCreateCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// repoCreateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
